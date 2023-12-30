5ff563f6f01a0134ad7210f34c3cc6398c31c9b9
Ly Luong <thikhanhly.luong@student.kuleuven.be>
2023-12-30 19:12:04 +0700

    fix meta

--- src/layouts/Meta.svelte
+++ src/layouts/Meta.svelte
@@ -2,7 +2,6 @@
   export let title = "Global Human Day - IDV - Group 6";
   export let description =
     "A new study calculated the average “global human day,” revealing which activities take up most of our time";
-  export let url;
   export let keywords = "global, idv, human, data";
 </script>
 
@@ -17,14 +16,11 @@
     property="og:site_name"
     content="Global Human Day - IDV 2023 - KU Leuven"
   />
-  <meta property="og:url" content={url} />
   <meta property="og:description" content={description} />
   <meta property="og:type" content="article" />
   <meta property="og:locale" content="en_US" />
 
   <meta name="robots" content="max-image-preview:large" />
-
-  <link rel="canonical" href="{url}/" />
 
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
--- svelte.config.js
+++ svelte.config.js
@@ -3,35 +3,32 @@ import adapterStatic from "@sveltejs/adapter-static";
 import sveltePreprocess from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
 
+
 const { subdirectory } = JSON.parse(readFileSync("package.json", "utf8"));
-const dev = process.env.NODE_ENV !== "production";
 const dir = subdirectory || "";
-const prefix = dir.startsWith("/") ? "" : "/";
-const base = dev || !dir ? "" : `${prefix}${dir}`;
 
 const preprocess = sveltePreprocess({
-  postcss: {
-    plugins: [autoprefixer],
-  },
+	postcss: {
+		plugins: [autoprefixer]
+	}
 });
 
 const config = {
-  preprocess,
-  kit: {
-    adapter: adapterStatic({
-      strict: false,
-    }),
-    alias: {
-      "components/*": "src/components/*",
-      "data/*": "src/data/*",
-      "routes/*": "src/routes/*",
-      "layouts/*": "src/layouts/*",
-      "pages/*": "src/pages/*",
-      "styles/*": "src/styles/*",
-      "svg-icons/*": "src/svg-icons/*",
-      "utils/*": "src/utils/*",
-    },
-  },
+	preprocess,
+	kit: {
+		adapter: adapterStatic(),
+		alias: {
+			"components/*": "src/components/*",
+			"data/*": "src/data/*",
+			"routes/*": "src/routes/*",
+			"layouts/*": "src/layouts/*",
+			"pages/*": "src/pages/*",
+			"styles/*": "src/styles/*",
+			"svg-icons/*": "src/svg-icons/*",
+			"utils/*": "src/utils/*"
+		}
+		
+	},
 };
 
 export default config;
