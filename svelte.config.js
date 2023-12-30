627dcd5b3933f0d7fc1ea9ad630c18547b99de53
Ly Luong <thikhanhly.luong@student.kuleuven.be>
2023-12-30 19:21:31 +0700

    update for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -3,32 +3,32 @@ import adapterStatic from "@sveltejs/adapter-static";
 import sveltePreprocess from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
 
-
 const { subdirectory } = JSON.parse(readFileSync("package.json", "utf8"));
+const dev = process.env.NODE_ENV !== "production";
 const dir = subdirectory || "";
+const prefix = dir.startsWith("/") ? "" : "/";
 
 const preprocess = sveltePreprocess({
-	postcss: {
-		plugins: [autoprefixer]
-	}
+  postcss: {
+    plugins: [autoprefixer],
+  },
 });
 
 const config = {
-	preprocess,
-	kit: {
-		adapter: adapterStatic(),
-		alias: {
-			"components/*": "src/components/*",
-			"data/*": "src/data/*",
-			"routes/*": "src/routes/*",
-			"layouts/*": "src/layouts/*",
-			"pages/*": "src/pages/*",
-			"styles/*": "src/styles/*",
-			"svg-icons/*": "src/svg-icons/*",
-			"utils/*": "src/utils/*"
-		}
-		
-	},
+  preprocess,
+  kit: {
+    adapter: adapterStatic({ paths: { base: dev ? "" : "/idv-2023-group-6" } }),
+    alias: {
+      "components/*": "src/components/*",
+      "data/*": "src/data/*",
+      "routes/*": "src/routes/*",
+      "layouts/*": "src/layouts/*",
+      "pages/*": "src/pages/*",
+      "styles/*": "src/styles/*",
+      "svg-icons/*": "src/svg-icons/*",
+      "utils/*": "src/utils/*",
+    },
+  },
 };
 
 export default config;
